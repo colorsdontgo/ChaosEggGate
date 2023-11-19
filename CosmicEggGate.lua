e86ca484953c390e122b60261743882be65eedb6
colorsdontgo <ellawhirled@gmail.com>
2023-11-18 17:16:07 -0800

    Fix CosmicEggGate
    The original way I wrote this didn't actually work properly. Instead, use a setup function that spawns a gate once in the first eligible chamber after equipping the Cosmic Egg. Also reset the control at the beginning of a run

--- CosmicEggGate.lua
+++ CosmicEggGate.lua
@@ -1,14 +1,32 @@
 ModUtil.Mod.Register("CosmicEggGate")
 
 local config = {
-    Enabled = true
+    Enabled = true,
+    CreatedGate = false
 }
 CosmicEggGate.config = config
 
-DebugPrint({Text = "CosmicEggGate: merging table"})
-ModUtil.Table.Merge(TraitData, {
-    ChaosBoonTrait = {
-        RemainingUses = 1,
-		ForceSecretDoor = true,
-    }
-})
+DebugPrint({Text = "CosmicEggGate: Adding setup function"})
+
+TraitData.ChaosBoonTrait.SetupFunction = {
+    Name = "CosmicEggGateSetup"
+}
+
+function CosmicEggGateSetup()
+    local secretPointIds = GetIdsByType({ Name = "SecretPoint" })
+    if CurrentRun ~= nil and CurrentRun.CurrentRoom ~= nil then
+        if not IsEmpty( secretPointIds ) and IsGameStateEligible( CurrentRun, CurrentRun.CurrentRoom.SecretDoorRequirements ) and not config.CreatedGate then
+            DebugPrint({ Text = "CosmicEggGate: forcing a secret door" })
+            config.CreatedGate = true
+            CurrentRun.CurrentRoom.ForceSecretDoor = true
+        end
+    end
+end
+
+OnAnyLoad{ "RoomPreRun",
+    function( triggerArgs )
+        if config.CreatedGate then
+            config.CreatedGate = false
+        end
+    end
+}
